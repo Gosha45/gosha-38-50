f216de427318c53d1fedfac1eee79a862015bf60
Gosha45 <122792632+Gosha45@users.noreply.github.com>
2023-02-07 14:01:38 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,114 @@
+"use strict"
+// 38
+// let arr1 = ['1','2','3']
+// let arr2 = ['a', 'b', 'c']
+// console.log(arr2)
+
+// 39
+// 1
+// let arr = ['1','2','3']
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// 2
+// let arr = [1, 2, 3]
+// console.log(arr[0],arr[1],arr[2])
+// 3
+// let arr = [1, 2, 3]
+// console.log(arr[0]+arr[1]+arr[2]);
+// 4
+// let arr = ['a','b','c','d']
+// console.log(arr[0],"+",arr[1],"+",arr[2],"+",arr[3])
+
+// 40
+// let arr = ['a','b','c','d','e'];
+// console.log(arr[arr.length - 1])
+
+// let arr = [1, 2, 3];
+// console.log(arr[arr.length - 1])
+
+// 41
+// let arr = ['a','b','c'];
+// arr[0] = '1';
+// arr[1] = '-2';
+// arr[2] = '-3';
+// console.log(arr);
+
+// let arr = [1, 2, 3];
+// arr[0] += '3';
+// arr[1] += '3';
+// arr[2] += '3';
+// console.log(arr);
+
+// let arr = [1, 2, 3];
+// arr[0]++;
+// arr[1]++;
+// arr[2]++;
+// console.log(arr);
+
+// 42
+
+// let arr = [];
+// arr[0] = '1';
+// arr[1] = '2';
+// arr[2] = '3';
+// console.log(arr);
+
+// let arr = [1,2,3];
+// arr[3] = 4;
+// arr[4] = 5;
+// console.log(arr);
+
+// 43
+
+// let arr = [];
+// arr[3] = 'a';
+// arr[8] = '8';
+// console.log(arr.length);
+
+// 44
+
+// let arr = [];
+// arr[0] = 1;
+// arr[1] = 2;
+// arr[2] = 3;console.log(arr);
+
+// let arr = [1, 2, 3];
+// arr[3] = 4;
+// arr[4] = 5;
+// console.log(arr);
+
+// 45
+
+// let arr = [1, 2, 3, 4, 5,];
+// let key = 2;
+// console.log(arr[key]);
+
+// let arr = [1, 2, 3, 4, 5];
+// let key1 = 1;
+// let key2 = 2;
+// console.log(arr[key1], arr[key2]);
+
+// 47
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[4]);
+
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+// 48
+
+// let obj = {1: 'ПН', 2: 'ВТ', 3: 'СР', 4: 'ЧТ', 5: 'ПТ', 6: 'СБ', 7: 'ВС'};
+// console.log(obj[1], obj[2], obj[3], obj[4], obj[5],  obj[6], obj[7]);
+
+// 49
+
+let obj = {1: 'январь', 2: 'февраль', 3: 'март', 4: 'апрель', 5: 'май', 6: 'июнь', 7: 'июль', 8: 'август', 9: 'сентябрь', 10: 'октябрь', 11: 'ноябрь', 12: 'декабрь'};
